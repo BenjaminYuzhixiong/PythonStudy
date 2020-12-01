d42e20453a8e42ccddc6b0ff8fb115d9ea671434
BenjaminYuzhixiong <29025728@qq.com>
2020-12-01 13:29:18 +0800

    Create 01inputs.py
    added if/else

---
+++ 01inputs.py
@@ -0,0 +1,17 @@
+#! /usr/bin/env python 
+# 会去环境设置寻找 python 目录
+# -*- coding: UTF-8 -*-
+
+# . if/else
+
+print ("Hello, Python!")
+
+
+val = 10
+
+if  val < 5: 
+   print(val , "<5")
+elif  val < 15:  
+   print(val , "<15")
+else :  
+   print(val , ">=15")
